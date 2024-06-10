9e461b72eeb030153d7454bf4b77c381026146bc
omar faruk <farukomarcpa@gmail.com>
2024-06-10 14:40:21 +0600

    404 updated

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,7 +2,7 @@
 
 const NotFound = () => {
     return (
-        <div>
+        <div className="text-center flex flex-col justify-center items-center">
             <h2 className="text-4xl">404</h2>
             <h3 className="text-2xl">Not Found Page</h3>
         </div>
